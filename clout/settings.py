af4ca8a907a6c6d3da1c81860b0dcb5581d9cf3b
emirmubze <emirmubze@gmail.com>
2026-08-25 00:37:40 +0530

    Configure Supabase S3 compatibility

--- clout/settings.py
+++ clout/settings.py
@@ -239,9 +239,14 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-east-1")
+    AWS_S3_REGION_NAME = os.getenv(
+        "AWS_S3_REGION_NAME",
+        "ap-northeast-1"
+    )
     AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
 
+    AWS_S3_SIGNATURE_VERSION = "s3v4"
+    AWS_S3_ADDRESSING_STYLE = "path"
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
     AWS_QUERYSTRING_AUTH = False
@@ -255,6 +260,8 @@ if USE_S3:
                 "bucket_name": AWS_STORAGE_BUCKET_NAME,
                 "endpoint_url": AWS_S3_ENDPOINT_URL,
                 "region_name": AWS_S3_REGION_NAME,
+                "signature_version": "s3v4",
+                "addressing_style": "path",
                 "file_overwrite": False,
                 "querystring_auth": False,
             },
