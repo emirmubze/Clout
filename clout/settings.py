ad46cf317265b0a5ff910ad2fe05428ab0b443a5
emirmubze <emirmubze@gmail.com>
2026-09-01 15:41:24 +0530

    Configure full Gmail SMTP settings and strip spaces from EMAIL_HOST_PASSWORD

--- clout/settings.py
+++ clout/settings.py
@@ -425,19 +425,10 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 # EMAIL
 # =========================================================
 
-EMAIL_BACKEND = os.getenv(
-    "EMAIL_BACKEND",
-    "django.core.mail.backends.smtp.EmailBackend"
-    if os.getenv("EMAIL_HOST_PASSWORD")
-    else "django.core.mail.backends.console.EmailBackend",
-)
-
-
 EMAIL_HOST = os.getenv(
     "EMAIL_HOST",
     "smtp.gmail.com",
 )
-
 
 EMAIL_PORT = int(
     os.getenv(
@@ -445,7 +436,6 @@ EMAIL_PORT = int(
         "587",
     )
 )
-
 
 EMAIL_USE_TLS = (
     os.getenv(
@@ -463,18 +453,22 @@ EMAIL_USE_SSL = (
     == "true"
 )
 
-
 EMAIL_HOST_USER = os.getenv(
     "EMAIL_HOST_USER",
     "clout.courses@gmail.com",
 )
 
-
 EMAIL_HOST_PASSWORD = os.getenv(
     "EMAIL_HOST_PASSWORD",
     "",
-)
+).replace(" ", "").strip()
 
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend"
+    if EMAIL_HOST_PASSWORD
+    else "django.core.mail.backends.console.EmailBackend",
+)
 
 DEFAULT_FROM_EMAIL = os.getenv(
     "DEFAULT_FROM_EMAIL",
